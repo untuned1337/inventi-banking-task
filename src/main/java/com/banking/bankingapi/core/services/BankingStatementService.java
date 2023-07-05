3edd067fe20a3da4dcebc88b4882445597496223
Rokas Sidaravičius <rokas.sidaravicius@presentconenction.eu>
2023-07-05 16:25:49 +0300

    feature: BankingStatementService

---
+++ src/main/java/com/banking/bankingapi/core/services/BankingStatementService.java
@@ -0,0 +1,85 @@
+package com.banking.bankingapi.core.services;
+
+import com.banking.bankingapi.core.dto.bankingStatements.BankingStatementResponse;
+import com.banking.bankingapi.core.dto.bankingStatements.CreateBankingStatementRequest;
+import com.banking.bankingapi.core.dto.common.DateRangeRequest;
+import com.banking.bankingapi.core.dto.common.FileResponse;
+import com.banking.bankingapi.core.extensions.ModelMapperExtensions;
+import com.banking.bankingapi.core.repositories.BankingStatementRepository;
+import com.banking.bankingapi.core.services.csv.CsvSerializer;
+import com.banking.bankingapi.domain.entities.BankingStatement;
+import org.modelmapper.ModelMapper;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
+
+@Service
+public class BankingStatementService {
+    private final BankingStatementRepository repository;
+    private final ModelMapper mapper;
+    private final CsvSerializer csvSerializer;
+    private final CurrencyConverter currencyConverter;
+
+    public BankingStatementService(
+            BankingStatementRepository repository,
+            ModelMapper mapper,
+            CsvSerializer serializer,
+            CurrencyConverter currencyConverter) {
+        this.repository = repository;
+        this.mapper = mapper;
+        this.csvSerializer = serializer;
+        this.currencyConverter = currencyConverter;
+    }
+
+    private List<BankingStatementResponse> mapResponses(List<BankingStatement> statements) {
+        return ModelMapperExtensions.mapList(mapper, statements, BankingStatementResponse.class);
+    }
+
+    public List<BankingStatementResponse> getAll() {
+        var statements = repository.getAll();
+        return mapResponses(statements);
+    }
+    
+    public FileResponse getCsv(DateRangeRequest dateRange) {
+        var statements = getInDateRange(dateRange);
+        var byteStream = csvSerializer.serialize(statements, BankingStatementResponse.class);
+        var inputStream = new InputStreamResource(byteStream);
+
+        var filename = "banking-statements.csv";
+
+        return new FileResponse(inputStream, filename, "application/csv");
+    }
+
+    public List<BankingStatementResponse> getInDateRange(DateRangeRequest dateRange) {
+        var statements = repository.getByFilter(
+                bankingStatement -> dateRange.includes(bankingStatement.getOperationDateTime())
+        );
+        return mapResponses(statements);
+    }
+
+    public void createMany(List<CreateBankingStatementRequest> requests) {
+        var statements = ModelMapperExtensions.mapList(mapper, requests, BankingStatement.class);
+        repository.createMany(statements);
+    }
+
+    public double getAccountBalance(String accountId, DateRangeRequest dateRange) {
+        var payedAmount = repository.getByFilter(
+            (statement) -> Objects.equals(statement.getAccountNumber(), accountId)
+                && dateRange.includes(statement.getOperationDateTime())
+        ).stream()
+                .map(statement -> currencyConverter.convertToEuros(statement.getAmount(), statement.getCurrency()))
+                .reduce(0.0, Double::sum);
+        var benefitedAmount = repository.getByFilter(
+            (statement) -> Objects.equals(statement.getBeneficiary(), accountId)
+                && dateRange.includes(statement.getOperationDateTime())
+        ).stream()
+                .map(statement -> currencyConverter.convertToEuros(statement.getAmount(), statement.getCurrency()))
+                .reduce(0.0, Double::sum);
+
+        var balance = benefitedAmount - payedAmount;
+
+        return MathHelpers.roundTo2decimalPlaces(balance);
+    }
+}
---
+++ src/test/java/com/banking/bankingapi/core/services/BankingStatementServiceTests.java
@@ -0,0 +1,141 @@
+package com.banking.bankingapi.core.services;
+
+import com.banking.bankingapi.core.dto.bankingStatements.BankingStatementResponse;
+import com.banking.bankingapi.core.dto.common.DateRangeRequest;
+import com.banking.bankingapi.core.extensions.ModelMapperExtensions;
+import com.banking.bankingapi.core.repositories.BankingStatementRepository;
+import com.banking.bankingapi.core.services.csv.CsvSerializer;
+import com.banking.bankingapi.domain.Currency;
+import com.banking.bankingapi.domain.entities.BankingStatement;
+import com.banking.bankingapi.infrastructure.repositories.InMemoryBankingStatementRepository;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.io.ByteArrayInputStream;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+
+@SpringBootTest
+public class BankingStatementServiceTests {
+    @MockBean
+    private BankingStatementRepository repository;
+    @MockBean
+    private CsvSerializer csvSerializer;
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Autowired
+    CurrencyConverter currencyConverter;
+
+    @BeforeEach
+    public void setUp() {
+        Mockito.when(repository.getAll()).thenReturn(dummyStatements);
+        Mockito
+            .when(csvSerializer.serialize(any(), eq(BankingStatementResponse.class)))
+            .thenReturn(new ByteArrayInputStream(new byte[6]));
+    }
+
+    private final List<BankingStatement> dummyStatements = List.of(
+            new BankingStatement(
+                    "LT12312312",
+                    LocalDateTime.of(2012, 4, 23, 6, 4),
+                    "LT66666666",
+                    "Some comment",
+                    50.5,
+                    Currency.EUR),
+            new BankingStatement(
+                    "LT12312312",
+                    LocalDateTime.of(2016, 4, 23, 6, 4),
+                    "LT66666666",
+                    "Some comment",
+                    50.5,
+                    Currency.EUR));
+
+    @Test
+    public void getAll_shouldReturnStatements() {
+        // Assert
+        var service = new BankingStatementService(repository, modelMapper, csvSerializer, currencyConverter);
+        var expectedStatements = ModelMapperExtensions.mapList(
+                modelMapper, dummyStatements, BankingStatementResponse.class);
+
+        // Act
+        var retrievedStatements = service.getAll();
+
+        // Assert
+        assertThat(retrievedStatements, Matchers.containsInAnyOrder(expectedStatements.toArray()));
+    }
+
+    @Test
+    public void getCsv_shouldReturnFileResponseWithCorrectFileName() {
+        // Arrange
+        var service = new BankingStatementService(repository, modelMapper, csvSerializer, currencyConverter);
+        var dateRange = new DateRangeRequest();
+        var expectedFileName = "banking-statements.csv";
+
+        // Act
+        var file = service.getCsv(dateRange);
+
+        // Assert
+        assertThat(file.getFilename(), Matchers.equalTo(expectedFileName));
+    }
+
+    @Test
+    public void getCsv_shouldReturnFileResponseWithCorrectContentType() {
+        // Arrange
+        var service = new BankingStatementService(repository, modelMapper, csvSerializer, currencyConverter);
+        var dateRange = new DateRangeRequest();
+        var expectedContentType = "application/csv";
+
+        // Act
+        var file = service.getCsv(dateRange);
+
+        // Assert
+        assertThat(file.getContentType(), Matchers.equalTo(expectedContentType));
+    }
+
+    @Test
+    public void getInDateRange_shouldReturnStatementsThatFallInProvidedDateRange() {
+        // Arrange
+        repository = new InMemoryBankingStatementRepository();
+        repository.createMany(dummyStatements);
+        var dateRange = new DateRangeRequest(
+                LocalDateTime.of(2011, 4, 23, 6, 4),
+                LocalDateTime.of(2013, 4, 23, 6, 4));
+        var service = new BankingStatementService(repository, modelMapper, csvSerializer, null);
+        var expectedStatements = ModelMapperExtensions.mapList(
+                modelMapper, dummyStatements.subList(0, 1), BankingStatementResponse.class);
+
+        // Act
+        var statements = service.getInDateRange(dateRange);
+
+        // Assert
+        assertThat(statements, Matchers.hasSize(expectedStatements.size()));
+    }
+
+    @Test
+    public void getAccountBalance_shouldReturnCorrectBalanceInEuros() {
+        // Arrange
+        repository = new InMemoryBankingStatementRepository();
+        repository.createMany(dummyStatements);
+        var accountId = "LT66666666";
+        var dateRange = new DateRangeRequest();
+        var service = new BankingStatementService(repository, modelMapper, csvSerializer, currencyConverter);
+        var expectedBalance = 101.0;
+
+        // Act
+        var balance = service.getAccountBalance(accountId, dateRange);
+
+        // Assert
+        assertThat(balance, Matchers.equalTo(expectedBalance));
+    }
+}
